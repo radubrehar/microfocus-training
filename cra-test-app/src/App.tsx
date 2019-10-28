148bb50ba2a281f68b10948d230c2b60e742c6b6
zpytchrb <radu.brehar@zippyui.com>
2019-10-29 00:21:05 +0200

    update

--- cra-test-app/src/App.tsx
+++ cra-test-app/src/App.tsx
@@ -7,18 +7,18 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
